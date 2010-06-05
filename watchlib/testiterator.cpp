87d5cb549cec7fa1c407fc01271d34ff952827e8
gohongyi <gohongyi@89f07240-0dd8-8067-447d-e4870be02bd0>
2010-06-05 10:12:08 +0000

    added some comment and fixed bug for adding into a blank

--- watchlib/testiterator.cpp
+++ watchlib/testiterator.cpp
@@ -22,6 +22,10 @@ vector<watchpoint_t>::iterator search_address(int target_addr, vector<watchpoint
 	return iter;
 }
 
+bool addr_covered (int target_addr, const watchpoint_t& node) {
+	return (target_addr >= node.addr && target_addr < node.addr + node.size);
+}
+
 bool watch_fault (int target_addr, int target_flags, const watchpoint_t& node) {
 	return (target_addr >= node.addr && target_addr < node.addr + node.size && target_flags & node.flags);
 }
@@ -31,7 +35,7 @@ int main () {
 	watchpoint_t temp;
 	int i;
 	for (i = 0; i < 5; i++) {
-		temp.addr = i*10;
+		temp.addr = (i+1)*10;
 		temp.size = 5;
 		temp.flags = 3;
 		wp.push_back(temp);
@@ -44,11 +48,17 @@ int main () {
 			break;
 	}
 */
-	iter = search_address(19, wp);
+	int target_addr;
+	target_addr = 0;
+	iter = search_address(target_addr, wp);
 	if (iter != wp.end()) {
 		cout << "The result addr is " << iter->addr << endl;
 		cout << "The result size is " << iter->size << endl;
 		cout << "The reuslt flag is " << iter->flags << endl;
+		if (addr_covered(target_addr, (*iter) ) )
+			cout << "The target_address is covered" << endl;
+		else
+			cout << "The target_address is not covered" << endl;
 	}
 	else
 		cout << "The result is Beyond the data structure" << endl;
