b7f575f9cc3e3d88120ee4a7c7a42e0b52537804
gohongyi <gohongyi@89f07240-0dd8-8067-447d-e4870be02bd0>
2010-06-16 21:27:30 +0000

    The library is now built

--- watchlib/auto_wp.h
+++ watchlib/auto_wp.h
@@ -6,43 +6,51 @@
 //													//
 //													//
 //////////////////////////////////////////////////////
+#ifndef VECTOR_H
+#include <vector>
+#define VECTOR_H
+#endif
 
-int open_ctl (char * file_path);
-//MODIFIES:
-//EFFECTS:
-//
+namespace {
+	#define	WA_READ			1
+	#define	WA_WRITE		2
+	
+	struct watchpoint_t {
+		int addr;
+		int size;
+		int flags;
+	};
+}
 
-void close_ctl (int ctlfd);
-//MODIFIES:
-//EFFECTS:
-//
+using std::vector;
 
-void set_byte (int target_addr, int target_wflags, int ctlfd);
-//INPUTS:	target_addr specifies where you want to put the watchpoint, and target_wflags specifies which flag you want to add; ctlfd determine which
-//			process you are adding.
-//MODIFIES: Watchpoint array (by writing into ctl file)
-//EFFECTS:	Add a watchpoint on a single byte. If the watchpoint already exist but have a different flag, then it will add the target_flag by ORing them
-//			If the flag is the same, then do noting.
-//SPECIAL NOTICE:	It does automatic splitting if it changes a byte of flag in the middle of a wide watchpoint.
-//					It also automatic merging the watchpoints if the newly added watchpoints is adjacent to the watchpoint before it or right after it.
+namespace Hongyi_WatchPoint {
+	class WatchPoint {
+	public:
+		//Constructors
+		WatchPoint();
+		WatchPoint(int target_addr, int target_size, int target_flags);
+		WatchPoint(const WatchPoint& parameter);
 
-//Below are basicly the same with set_byte only with a difference of range(2bytes, 4bytes & 8bytes).
-void set_2byte (int target_addr, int target_wflags, int ctlfd);
-void set_4byte (int target_addr, int target_wflags, int ctlfd);
-void set_8byte (int target_addr, int target_wflags, int ctlfd);
+		
+		void add_read_wp (int target_addr, int target_size);
+		void add_write_wp (int target_addr, int target_size);
+		
+		void rm_watchpoint (int target_addr, int target_size);
+		
+		int watch_fault (int target_addr, int target_size);
+		//return: The number of how many watchpoints it touches within the range, regardless what kind of flags the watchpoint has.
+		int read_fault(int target_addr, int target_size);
+		//return: The number of how many *read* watchpoints it touches within the range.
+		int write_fault(int target_addr, int target_size);
+		//return: The number of how many *write* watchpoints it touches within the range.
+		
+		void watch_print();
+		
+		void add_watchpoint (int target_addr, int target_size, int target_flags);
+		int general_fault (int target_addr, int target_size, int target_flags);
+	private:
+		vector<watchpoint_t> wp;
+	};
+}
 
-void rm_range (int target_addr, int target_size, int ctlfd);
-//INPUTS:	target_addr specifies where you want to put the watchpoint, and target_size specifies the range you want to remove; ctlfd determine which
-//			process you are adding.
-//MODIFIES:	Watchpoint array (by writting into ctl file)
-//EFFECTS:	It remove all the watchpoints staring with target_addr, with a intervle of size.
-
-void set_range (int target_addr, int target_size, int target_wflags, int ctlfd);
-//MODIFIES: Watchpoint array (by writing into ctl file)
-//EFFECTS:	Add a watchpoint on target_addr with a length of target_size. If the watchpoint already exist but have a different flag, then it will add the target_flag by ORing them. If the flag is the same, then do noting.
-//SPECIAL NOTICE:	It does automatic splitting if it changes a byte of flag in the middle of a wide watchpoint.
-//					It also automatic merging the watchpoints if the newly added watchpoints is adjacent to the watchpoint before it or right after it.
-
-void watch_print();
-//MODIFIE:	Nothing
-//EFFECTS:	Print out all the watchpoints to the screen.
