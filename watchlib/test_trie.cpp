17b51399cba303e9168d2b268d925573ef612c33
menacer <menacer@89f07240-0dd8-8067-447d-e4870be02bd0>
2010-07-24 21:58:24 +0000

    Changed test_trie.cpp to compile with new long long values.

--- watchlib/test_trie.cpp
+++ watchlib/test_trie.cpp
@@ -10,9 +10,9 @@ using namespace std;
 using namespace Hongyi_WatchPoint;
 
 int main () {
-	unsigned int top_hit = 0;
-	unsigned int mid_hit = 0;
-	unsigned int bot_hit = 0;
+	unsigned long long top_hit = 0;
+	unsigned long long mid_hit = 0;
+	unsigned long long bot_hit = 0;
 	unsigned int target_addr;
 	unsigned int target_size;
 	
@@ -26,7 +26,7 @@ int main () {
 	watch.add_watch_wp (target_addr, target_size);
 	
 	cout << "Below is for general_fault" << endl;
-	if ( watch.general_fault(4194302, 8, 3, top_hit, mid_hit, bot_hit) ) {
+	if ( watch.general_fault(4194302, 8, 3, top_hit, mid_hit, bot_hit, false) ) {
 		cout << "top_hit: " << top_hit << endl;
 		cout << "mid_hit: " << mid_hit << endl;
 		cout << "bot_hit: " << bot_hit << endl;
